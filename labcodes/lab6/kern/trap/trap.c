e3d79d3c026ed05280b98661c22351d422faa159
931746592 <931746592@qq.com>
2016-05-23 19:51:14 +0800

    lab6 finish

--- labcodes/lab6/kern/trap/trap.c
+++ labcodes/lab6/kern/trap/trap.c
@@ -57,6 +57,13 @@ idt_init(void) {
      /* LAB5 YOUR CODE */ 
      //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
      //so you should setup the syscall interrupt gate in here
+    extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++) {
+        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+    }
+    SETGATE(idt[T_SYSCALL], 1, GD_KTEXT, __vectors[T_SYSCALL], DPL_USER);
+    lidt(&idt_pd);
 }
 
 static const char *
@@ -229,6 +236,13 @@ trap_dispatch(struct trapframe *tf) {
          * IMPORTANT FUNCTIONS:
 	     * sched_class_proc_tick
          */
+        //  ticks ++;
+        // if (ticks % TICK_NUM == 0) {
+        //     assert(current != NULL);
+        //     current->need_resched = 1;
+        // }
+        ticks ++;
+        assert(current != NULL);
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
